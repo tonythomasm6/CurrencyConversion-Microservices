0726d4332a4a96ab8ce6744e32f9a15491d28388
Tony Thomas <ttho0017@student.monash.edu>
2021-08-12 00:08:56 +1000

    API Gateway setup complete

--- api-gateway/src/main/java/com/example/apigateway/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/example/apigateway/configuration/ApiGatewayConfiguration.java
@@ -8,10 +8,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApiGatewayConfiguration {
 
-//    @Bean
-//    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
-//        return builder.routes()
-//                .route(null)
-//                .build();
-//    }
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(p->p.path("/get")
+                        .filters(f->f.addRequestHeader("MyHeader","MyURI")
+                        .addRequestParameter("Param","MyValue"))
+                        .uri("http:/httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                .uri("lb://currency-conversion"))
+                .route(p->p.path("currency-conversion-new/**")
+                .filters(f->f.rewritePath("/currency-conversion-new/",
+                        "/currency-conversion-feign/"))
+                .uri("lb://currency-conversion"))
+                .build();
+    }
 }
